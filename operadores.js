273afb0044cd97ab54d18eb54044a46feaac9af9
llanesleonardo <llanesleonardo@gmail.com>
2022-10-09 16:44:27 -0700

    tema operadores

---
+++ operadores.js
@@ -0,0 +1,30 @@
+// Operadores binarios
+
+var v1 = 3 + 2;
+var v2 = 50 - 10;
+var v3 = 10 * 20;
+var v4 = 20 / 2;
+
+var v5 = "Leonardo" + " " + "Llanes";
+
+//Operadores unitarios
+var v6 = !false;
+var v7 = !true;
+
+var v8 = 1;
+
+// Operadores de comparación
+// 3 == "3";
+// 3 === "3";
+// 5 < 3;
+// 5 > 3;
+//5 <= 6;
+// 5 >= 6;
+
+// a && b
+// a || b
+//
+
+var edad = 40;
+edad++;
+edad += 2;

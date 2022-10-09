f2df5ba3a103ed0ba53d1f6cbccb5098b694ae18
llanesleonardo <llanesleonardo@gmail.com>
2022-10-09 15:09:45 -0700

    temas valores y  variables

---
+++ valores.js
@@ -0,0 +1,34 @@
+// Número: 40
+
+// String: "Diego de Granda"
+
+// Boolean:
+// True
+// False
+
+// Vacios:
+// null
+// undefined
+
+// Arreglos:
+// [1,2,3]
+
+// Objeto:
+// {nombre: "Leonardo"}
+
+// typeof 40
+// 'number'
+// typeof "Leonardo"
+// 'string'
+// typeof Oscar
+// 'undefined'
+// typeof true
+// 'boolean'
+// typeof null
+// "object"
+// typeof undefined
+// "undefined"
+// typeof []]
+// "object"
+// typeof {}
+// "object"
---
+++ variables.js
@@ -0,0 +1,18 @@
+//var tengo que reservar un espacio en memoria para guardar un valor
+var nombre = "Leonardo";
+
+//declarar
+var edad;
+
+//inicializar
+edad = 17;
+
+//Variable arreglo
+var elementos = ["Computadora", "Celular"];
+
+var objeto_persona = {
+    nombre: "Leonardo"
+    apellido:"Llanes"
+}
+
+objeto_persona[1];

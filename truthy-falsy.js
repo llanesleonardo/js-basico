0912550ca1d71cfc00fb747e2484bb0621eb979e
llanesleonardo <llanesleonardo@gmail.com>
2022-10-09 16:36:49 -0700

    tema truthyfalsy

---
+++ truthy-falsy.js
@@ -0,0 +1,10 @@
+var v1 = Boolean();
+var v2 = Boolean(0);
+var v3 = Boolean(NaN);
+var v4 = Boolean(undefined);
+var v5 = Boolean(" a");
+var v6 = Boolean("a");
+var v7 = Boolean(1);
+var v8 = Boolean([]);
+var v9 = Boolean({});
+var v10 = Boolean(function fun1(x) {});

904b96b1ca2bcb845bf05bd66baa728398ba32ad
llanesleonardo <llanesleonardo@gmail.com>
2022-10-09 19:18:30 -0700

    tema switch

---
+++ switch.js
@@ -0,0 +1,20 @@
+// Switch
+
+var x = 1;
+
+switch (x) {
+  case 1:
+    console.log("Soy uno");
+    break;
+
+  case 2:
+    console.log("Soy dos");
+    break;
+
+  default:
+    break;
+}
+
+// el break sirve para salir si es que se cumple na condición.
+
+// Si no se cumple un caso, enta al default que es un caso especial
